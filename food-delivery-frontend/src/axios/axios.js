bd8040d668bb6db4fa6b58b582c1b15d0be6bd34
Ljubica Jovanoska <138476065+jovanoskalj@users.noreply.github.com>
2025-06-09 11:39:51 +0200

    Update axios.js

--- food-delivery-frontend/src/axios/axios.js
+++ food-delivery-frontend/src/axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://20.42.81.109/api",
+    baseURL: "http://20.42.81.109:8081/api",
     headers: {
         "Content-Type": "application/json",
     },

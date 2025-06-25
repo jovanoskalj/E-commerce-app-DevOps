683e39274185cefee8926d542e69c5803a8c3123
Ljubica Jovanoska <lubicaj724@gmail.com>
2025-06-25 17:52:11 +0200

    Fixing CORS problem

--- food-delivery-frontend/src/axios/axios.js
+++ food-delivery-frontend/src/axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://20.42.81.109:8081/api",
+    baseURL: "http://4.232.137.90:8080",
     headers: {
         "Content-Type": "application/json",
     },
